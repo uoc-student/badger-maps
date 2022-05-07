399bf2893e34ae3605de757ca25c6e889c021adc
Sony Dev <sony_avil@protnmail.com>
2022-05-07 13:46:33 +0300

    func. connections to db OK

--- badger_query.py
+++ badger_query.py
@@ -3,16 +3,22 @@ import logging                      # log errors into a log file
 import mysql.connector              # 
 from mysql.connector import Error   # exception handler
 
-"""
-hostname: localhost
-user: badger
-pass: maps
-db: badger_maps
-"""
 
 # Function to establish a conncetion with the db
 def create_server_connection(host_name, user_name, user_password, user_db):
-    pass
+    conneciton = None
+    try:
+        connection = mysql.connector.connect(
+            host = host_name,
+            user = user_name,
+            password = user_password,
+            database = user_db
+            )
+        print("MySQL connection with " + user_db + " by " + user_name + " established successfully!")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
 
 # Create a function to generate a database
 def create_database(connection, query):
@@ -26,13 +32,31 @@ def execute_query(connection, query):
 def print_query_result(query_obj):
     pass
 
+
+# Funtion to read from csv (parser)
+def read_from_csv_to_db_table(connection, csv_dir):
+    pass
+
+
 # Queries
+# Most recent check-in
+# Least recent check-in
+# Customer list sorted alphabetically
 
 
 
 # Main
 def main():
-    pass
+
+    hostname = "localhost"
+    user = "badger"
+    password = "maps"
+    database = "badger_db"
+
+    # Establish connection with the database
+    connection = create_server_connection(hostname, user, password, database)
+
+    
 
 
 if __name__ == "__main__":
